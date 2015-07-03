31d0f6a5412d6c9768b7b64877c0a6ba43eb7c6d
Bram Wubs <bwubs@me.com>
2015-07-03 13:18:47 +0200

    Added error logging methods to the Logger interface

--- src/Contracts/Logger.php
+++ src/Contracts/Logger.php
@@ -4,7 +4,18 @@
 namespace Robin\Support\Contracts;
 
 
+use Robin\Api\Models\Customer;
+use Robin\Api\Models\Order;
+
 interface Logger
 {
     public function info($string);
+
+    public function customerMakeError(Customer $customer, $reason, $primitive);
+
+    public function customerSendError(Customer $customer, $statusCode, $primitive);
+
+    public function orderMakeError(Order $customer, $reason, $primitive);
+
+    public function orderSendError(Order $customer, $statusCode, $primitive);
 }
